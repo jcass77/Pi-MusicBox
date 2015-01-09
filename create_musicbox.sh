8c31b4d4278de7f60f13735ae09d09ff19b2628d
jcass <john.cass77@gmail.com>
2015-01-09 09:18:56 +0200

    Rebase on /upstream/develop.

--- create_musicbox.sh
+++ create_musicbox.sh
@@ -7,12 +7,12 @@
 echo -e 'APT::Install-Recommends "0";\nAPT::Install-Suggests "0";\n' > /etc/apt/apt.conf
 
 #Install the packages you need to continue:
-apt-get update && apt-get --yes install sudo wget unzip mc
+#apt-get update && apt-get --yes install sudo wget unzip mc rsync
 
 #Next, issue this command to update the distribution.
 #This is good because newer versions have fixes for audio and usb-issues:
 
-apt-get dist-upgrade -y
+#apt-get dist-upgrade -y
 
 #Next, configure the installation of Mopidy, the music server that is the heart of MusicBox.
 #wget -q -O - http://apt.mopidy.com/mopidy.gpg | sudo apt-key add -
@@ -22,23 +22,26 @@ apt-get dist-upgrade -y
 ntpdate -u ntp.ubuntu.com
 
 #Then install all packages we need with this command:
-sudo apt-get update && sudo apt-get --yes --no-install-suggests --no-install-recommends install logrotate alsa-utils wpasupplicant gstreamer0.10-alsa ifplugd gstreamer0.10-fluendo-mp3 gstreamer0.10-tools samba dos2unix avahi-utils alsa-base cifs-utils avahi-autoipd libnss-mdns ntpdate ca-certificates ncmpcpp rpi-update linux-wlan-ng alsa-firmware-loaders iw atmel-firmware firmware-atheros firmware-brcm80211 firmware-ipw2x00 firmware-iwlwifi firmware-libertas firmware-linux firmware-linux-nonfree firmware-ralink firmware-realtek zd1211-firmware linux-wlan-ng-firmware alsa-firmware-loaders iptables build-essential python-dev python-pip python-gst0.10 gstreamer0.10-plugins-good gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-tools usbmount monit upmpdcli watchdog dropbear mpc dosfstools logrotate
+#sudo apt-get update && sudo apt-get --yes --no-install-suggests --no-install-recommends install logrotate alsa-utils wpasupplicant gstreamer0.10-alsa ifplugd gstreamer0.10-fluendo-mp3 gstreamer0.10-tools samba dos2unix avahi-utils alsa-base cifs-utils avahi-autoipd libnss-mdns ntpdate ca-certificates ncmpcpp rpi-update linux-wlan-ng alsa-firmware-loaders iw atmel-firmware firmware-atheros firmware-brcm80211 firmware-ipw2x00 firmware-iwlwifi firmware-libertas firmware-linux firmware-linux-nonfree firmware-ralink firmware-realtek zd1211-firmware linux-wlan-ng-firmware alsa-firmware-loaders iptables build-essential python-dev python-pip python-gst0.10 gstreamer0.10-plugins-good gstreamer0.10-plugins-bad gstreamer0.10-plugins-ugly gstreamer0.10-tools usbmount monit upmpdcli watchdog cifs-utils dropbear mpc dosfstools logrotate
 
 #mopidy from pip
-sudo pip install -U mopidy mopidy-spotify mopidy-local-sqlite mopidy-local-whoosh mopidy-scrobbler mopidy-soundcloud mopidy-dirble mopidy-tunein mopidy-gmusic mopidy-subsonic mopidy-http-kuechenradio mopidy-moped mopidy-musicbox-webclient mopidy-websettings mopidy-internetarchive mopidy-podcast mopidy-podcast-itunes mopidy-podcast-gpodder.net mopidy-simplewebclient mopidy-somafm mopidy-spotify-tunigo mopidy-youtube
+#sudo pip install -U mopidy mopidy-spotify mopidy-local-sqlite mopidy-local-whoosh mopidy-scrobbler mopidy-soundcloud mopidy-dirble mopidy-tunein mopidy-gmusic mopidy-subsonic mopidy-http-kuechenradio mopidy-moped mopidy-musicbox-webclient mopidy-websettings mopidy-internetarchive mopidy-podcast mopidy-podcast-itunes mopidy-podcast-gpodder.net mopidy-simplewebclient mopidy-somafm mopidy-spotify-tunigo mopidy-youtube
 
 #Google Music works a lot better if you use the development version of mopidy-gmusic:
-sudo pip install https://github.com/hechtus/mopidy-gmusic/archive/develop.zip
+#sudo pip install https://github.com/hechtus/mopidy-gmusic/archive/develop.zip
 
 #**Configuration and Files**
 cd /opt
 
 #Get the files of the Pi MusicBox project
-wget https://github.com/woutervanwijk/Pi-MusicBox/archive/master.zip
+#wget https://github.com/woutervanwijk/Pi-MusicBox/archive/master.zip
+
+# Use localised version instead
+rsync -r --links jcass@192.168.0.32:/Users/jcass/PycharmProjects/Mopidy/Pi-MusicBox/ ./Pi-MusicBox-master/
 
 #Unpack the zip-file and remove it if you want.
-unzip master.zip
-rm master.zip
+#unzip master.zip
+#rm master.zip
 
 #Then go to the directory which you just unpacked, subdirectory ‘filechanges’:
 cd Pi-MusicBox-master/filechanges
@@ -47,7 +50,7 @@ cd Pi-MusicBox-master/filechanges
 #This sets up the boot and opt directories:
 #manually copy cmdline.txt and config.txt if you want
 mkdir /boot/config
-cp -R boot/config /boot/config
+cp -R boot/* /boot
 cp -R opt/* /opt
 
 #Make the system work:
@@ -58,12 +61,12 @@ chown root:root /etc/firewall/musicbox_iptables
 chmod 600 /etc/firewall/musicbox_iptables
 
 #Next, create a symlink from the package to the /opt/defaultwebclient.
-ln -fsn /usr/local/lib/python2.7/dist-packages/mopidy_musicbox_webclient/static /opt/webclient
+ln -fsn /var/lib/git/Mopidy-MusicBox-Webclient/mopidy_musicbox_webclient/static /opt/webclient
 ln -fsn /opt/webclient /opt/defaultwebclient
 
 #Remove the streamuris.js and point it to the file in /boot/config
-mv /usr/local/lib/python2.7/dist-packages/mopidy_musicbox_webclient/static/js/streamuris.js streamuris.bk
-ln -fsn /boot/config/streamuris.js /usr/local/lib/python2.7/dist-packages/mopidy_musicbox_webclient/static/js/streamuris.js
+mv /var/lib/git/Mopidy-MusicBox-Webclient/mopidy_musicbox_webclient/static/js/streamuris.js streamuris.bk
+ln -fsn /boot/config/streamuris.js /var/lib/git/Mopidy-MusicBox-Webclient/mopidy_musicbox_webclient/static/js/streamuris.js
 
 #Let everyone shutdown the system (to support it from the webclient):
 chmod u+s /sbin/shutdown
@@ -94,7 +97,7 @@ chmod -R 777 /music
 chown -R mopidy:mopidy /music
 
 #Disable the SSH service for more security if you want (it can be started with an option in the configuration-file):
-update-rc.d ssh disable
+#update-rc.d ssh disable
 
 #Link the mopidy configuration to the new one in /boot/config
 ln -fsn /boot/config/settings.ini /home/mopidy/.config/mopidy/mopidy.conf
@@ -113,7 +116,7 @@ ln -fsn /boot/config/settings.ini /var/lib/mopidy/.config/mopidy/mopidy.conf
 sed -i '1s/^/dwc_otg.fiq_fix_enable=1 dwc_otg.fiq_split_enable=0 smsc95xx.turbo_mode=N /' /boot/cmdline.txt
 
 #cleanup
-apt-get remove build-essential python-pip
+#apt-get remove build-essential python-pip
 apt-get autoremove
 apt-get clean
 apt-get autoclean
